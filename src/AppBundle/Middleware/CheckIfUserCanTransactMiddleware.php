9513bbc2a67bed1fc6c3354bed94ed83e702c2f9
Arley Oliveira <arley.msn@hotmail.com>
2021-09-18 11:40:04 -0300

    Cria middlewares para checkar se é possível realizar a transação

---
+++ src/AppBundle/Middleware/CheckIfUserCanTransactMiddleware.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Middleware;
+
+use AppBundle\Entity\LegalUser;
+use AppBundle\Entity\PersonUser;
+use AppBundle\Exceptions\AbstractException;
+use AppBundle\Exceptions\Factories\ExceptionFactory;
+use AppBundle\Exceptions\InvalidUserException;
+
+class CheckIfUserCanTransactMiddleware extends Middleware
+{
+    /**
+     * @var PersonUser
+     */
+    private $user;
+
+    /**
+     * @param PersonUser $user
+     */
+    public function __construct(PersonUser $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return bool
+     * @throws AbstractException
+     */
+    public function check(): bool
+    {
+        if ($this->user instanceof LegalUser) {
+            throw ExceptionFactory::create(
+                InvalidUserException::class,
+                "Este usuário não pode enviar dinheiro!"
+            );
+        }
+
+        return $this->checkNext();
+    }
+
+    /**
+     * @return PersonUser
+     */
+    public function getUser(): PersonUser
+    {
+        return $this->user;
+    }
+}
---
+++ src/AppBundle/Middleware/CheckIfUserHasAvailableValueMiddleware.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Middleware;
+
+use AppBundle\Entity\Wallet;
+use AppBundle\Exceptions\Factories\ExceptionFactory;
+use AppBundle\Exceptions\InvalidTransactionException;
+
+class CheckIfUserHasAvailableValueMiddleware extends Middleware
+{
+    /**
+     * @var Wallet
+     */
+    private $wallet;
+
+    /**
+     * @var float
+     */
+    private $requestValue;
+
+    /**
+     * @param Wallet $wallet
+     * @param float $requestValue
+     */
+    public function __construct(Wallet $wallet, float $requestValue)
+    {
+        $this->wallet = $wallet;
+        $this->requestValue = $requestValue;
+    }
+
+
+    public function check(): bool
+    {
+        if ($this->requestValue > $this->wallet->getAvailableValue()) {
+            throw ExceptionFactory::create(
+                InvalidTransactionException::class,
+                "Saldo indisponível!"
+            );
+        }
+
+        return $this->checkNext();
+    }
+
+    /**
+     * @return Wallet
+     */
+    public function getWallet(): Wallet
+    {
+        return $this->wallet;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRequestValue(): float
+    {
+        return $this->requestValue;
+    }
+}
